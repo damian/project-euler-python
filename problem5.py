cb215a53fb9437236f0ac63a0b78d83864035e5b
Damian Nicholson <damian.nicholson21@googlemail.com>
2009-08-02 23:03:45 +0100

    Also done problem 5

---
+++ problem5.py
@@ -0,0 +1,18 @@
+def divideByRange(n):
+	counter = 0
+	for i in range(1,20):
+		if (n%i==0):
+			counter += 1
+	return counter==19
+
+def problem5():
+	smallest = 0
+	i = 2520
+	while (i < 1000000000):
+		if (divideByRange(i) and smallest==0):
+			smallest = i
+		i += 2520
+	return smallest
+
+print "Answer = %d" % problem5()
+	
